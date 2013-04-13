6578d7efea27c9bc534e773b82ffd05a870eb8df
Daniel <daniel@jezuk.co.uk>
2013-04-13 12:47:02 +0100

    The map is now working (to some extent)

--- src/net/beaner/mapthatsplat/RoadkillFragment.java
+++ src/net/beaner/mapthatsplat/RoadkillFragment.java
@@ -1,5 +1,8 @@
 package net.beaner.mapthatsplat;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,11 +11,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class RoadkillFragment extends Fragment {
+	private MapView map_;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_roadkill, container, false);
 
+        map_ = (MapView)rootView.findViewById(R.id.mapview);
+        map_.setTileSource(TileSourceFactory.MAPNIK);
+        map_.setBuiltInZoomControls(true);
+        map_.setMultiTouchControls(true);
+        map_.
+        
         return rootView;
     }
 
